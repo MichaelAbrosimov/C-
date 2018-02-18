5f00c8b5083dc23b16158ac5a4781f76b3681a5c
Михаил Абросимов <michael.abrosimov@gillsoft.tech>
2018-02-18 08:40:29 +0200

    Initial Commit

---
+++ ClassProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ClassProject
+//
+//  Created by Михаил Абросимов on 18.02.2018.
+//  Copyright © 2018 Михаил Абросимов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
